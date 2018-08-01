91d411aac86f9ff26b8572621cf19238632068d6
Yury Stryhunou <ystryhunou@spectrumdt.com>
2018-08-01 13:38:46 -0400

    - Add communication error handling  - Move ApiCaller calls to the AsyncTask  - Make progress dialog uncancellable

--- app/src/main/java/com/paysafe/test/activity/InfoActivity.java
+++ app/src/main/java/com/paysafe/test/activity/InfoActivity.java
@@ -1,5 +1,9 @@
 package com.paysafe.test.activity;
 
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -29,6 +33,7 @@ public class InfoActivity extends AppCompatActivity {
     RecyclerView rvPersons;
 
     private long back_pressed;
+    private ProgressDialog progressDialog = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,47 +44,20 @@ public class InfoActivity extends AppCompatActivity {
     }
 
     private void getData() {
-        final List<Person> persons = new ArrayList<>();
-        final Customer[] customers;
-        final Employee[] employees;
-
-        customers = ApiCaller.getCustomers();
-        employees = ApiCaller.getEmployees();
-        persons.addAll(Arrays.asList(customers));
-        persons.addAll(Arrays.asList(employees));
-
-        Collections.sort(persons, new Comparator<Person>() {
-            @Override
-            public int compare(Person o1, Person o2) {
-                if (o1 == null && o2 != null)
-                    return -1;
-
-                if (o1 != null && o2 == null)
-                    return 1;
-
-                if (o1 == null && o2 == null)
-                    return 0;
-
-                if (o1.getName() == null && o2.getName() != null)
-                    return -1;
-
-                if (o1.getName() != null && o2.getName() == null)
-                    return 1;
-
-                if (o1.getName() == null && o2.getName() == null)
-                    return 0;
-
-                return o1.getName().compareToIgnoreCase(o2.getName());
-            }
-        });
-        setupList(persons);
+        showProgressDialog();
+        new GetPersonsList().execute();
     }
 
     private void setupList(final List<Person> persons) {
-        rvPersons.setLayoutManager(new LinearLayoutManager(this));
-        rvPersons.setAdapter(new PersonAdapter(this, persons));
+        if (persons == null) {
+            dismissProgressDialog();
+            showAlert();
+        } else {
+            rvPersons.setLayoutManager(new LinearLayoutManager(this));
+            rvPersons.setAdapter(new PersonAdapter(this, persons));
+            dismissProgressDialog();
+        }
     }
-
 
     @Override
     public void onBackPressed() {
@@ -89,5 +67,98 @@ public class InfoActivity extends AppCompatActivity {
             Toast.makeText(getBaseContext(), getString(R.string.infoActivity_confirmation), Toast.LENGTH_SHORT).show();
 
         back_pressed = System.currentTimeMillis();
+    }
+
+    private void showProgressDialog() {
+        if (progressDialog == null) {
+            progressDialog = new ProgressDialog(this);
+            progressDialog.setMessage(getString(R.string.infoActivity_loading));
+            progressDialog.setCancelable(false);
+        }
+
+        progressDialog.show();
+    }
+
+    private void dismissProgressDialog() {
+        if (progressDialog != null) {
+            progressDialog.dismiss();
+
+        }
+    }
+
+    private class GetPersonsList extends AsyncTask<Void, Void, List<Person>> {
+        @Override
+        protected List<Person> doInBackground(Void... voids) {
+            final List<Person> persons = new ArrayList<>();
+            final Customer[] customers;
+            final Employee[] employees;
+
+            showProgressDialog();
+            customers = ApiCaller.getCustomers();
+
+            if (customers == null)
+                return null;
+
+            employees = ApiCaller.getEmployees();
+
+            if (employees == null)
+                return null;
+
+            persons.addAll(Arrays.asList(customers));
+            persons.addAll(Arrays.asList(employees));
+
+            Collections.sort(persons, new Comparator<Person>() {
+                @Override
+                public int compare(Person o1, Person o2) {
+                    if (o1 == null && o2 != null)
+                        return 1;
+
+                    if (o1 != null && o2 == null)
+                        return -1;
+
+                    if (o1 == null && o2 == null)
+                        return 0;
+
+                    if (o1.getName() == null && o2.getName() != null)
+                        return 1;
+
+                    if (o1.getName() != null && o2.getName() == null)
+                        return -1;
+
+                    if (o1.getName() == null && o2.getName() == null)
+                        return 0;
+
+                    return o1.getName().compareToIgnoreCase(o2.getName());
+                }
+            });
+
+            return persons;
+        }
+
+        @Override
+        protected void onPostExecute(final List<Person> persons) {
+            setupList(persons);
+        }
+
+        @Override
+        protected void onPreExecute() {
+        }
+
+        @Override
+        protected void onProgressUpdate(Void... values) {
+        }
+    }
+
+    private void showAlert() {
+        AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+        alertDialog.setTitle(getString(R.string.infoActivity_alertTitle));
+        alertDialog.setMessage(getString(R.string.infoActivity_alertMsg));
+        alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, getString(R.string.infoActivity_ok),
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+        alertDialog.show();
     }
 }
