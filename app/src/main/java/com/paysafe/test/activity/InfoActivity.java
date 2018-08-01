cf8e80a7a746206d74fdb91f0c8afd42558522e1
Yury Stryhunou <ystryhunou@spectrumdt.com>
2018-08-01 11:35:25 -0400

    - Initial commit

---
+++ app/src/main/java/com/paysafe/test/activity/InfoActivity.java
@@ -0,0 +1,15 @@
+package com.paysafe.test.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.paysafe.test.R;
+
+public class InfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info);
+    }
+}
